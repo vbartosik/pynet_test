37858d740b0d797373a28d08738d15b309a93f02
vbartosik <bartosik@netcraft.solutions>
2017-03-31 15:51:12 -0700

    Added JSON write part

--- yamljson.py
+++ yamljson.py
@@ -4,5 +4,5 @@ data = range(6)
 data.append({'ipaddress':'192.168.0.1', 'snmpcommunity':'public'})
 with open('w1_yaml.yml','w') as f:
    f.write(yaml.dump(data, default_flow_style=False))
-with open('w1_yaml.json','w') as f:
+with open('w1_json.json','w') as f:
    json.dump(data, f)
